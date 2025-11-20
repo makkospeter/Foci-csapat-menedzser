5a6712e2352e5625fbddf2e932d06f040a0f69f8
makkos.peter <makkos.peter@students.jedlik.eu>
2025-11-20 10:44:47 +0100

    Add project files.

---
+++ Foci csapat menedzser/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Foci_csapat_menedzser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
