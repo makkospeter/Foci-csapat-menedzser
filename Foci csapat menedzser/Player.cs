43b5309b49f5e792cd907edf620c5c50cec20aeb
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-11-27 10:33:07 +0100

    játékos nemzetiség, mezszám, contract

--- Foci csapat menedzser/Player.cs
+++ Foci csapat menedzser/Player.cs
@@ -8,11 +8,18 @@ namespace Foci_csapat_menedzser
 {
     public class Player
     {
-        public string Name;
-        public int Age;
-        public DateTime BirthDate;
-        public string Nationality;
-        public int MarketValue;
-        public Availability IsAvailable;
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        private List<string> Nationalities {  get; set; }
+        public int MarketValue { get; set; }
+        public bool IsAvailable { get; set; }
+        public string UnavailableReason { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int JerseyNumber { get; set; }
+        public int Height { get; set; }
+        public string PreferredFoot { get; set; }
+        public string Position { get; set; }
+        public DateTime JoinedTeam { get; set; }
+        public DateTime ContractEnd { get; set; }
     }
 }
